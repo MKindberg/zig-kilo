92308b8bc6017569134020a5a3b95cfe23deaa1f
Marcus <6180175+MKindberg@users.noreply.github.com>
2023-12-27 21:13:37 +0100

    Draw ~ on screen

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,65 @@
 const std = @import("std");
-const cLibs = @cImport({@cInclude("stdlib.h"); @cInclude("termios.h");});
+const cLibs = @cImport({
+    @cInclude("stdlib.h");
+    @cInclude("termios.h");
+});
+
+// Constants
+const STDOUT = std.io.getStdOut();
+const escape = struct {
+    const esc = "\x1b";
+    const clear_screen = esc ++ "[2J";
+    const cursor_to_top = esc ++ "[H";
+    const cursor_to_right = esc ++ "[999C";
+    const cursor_to_bot = esc ++ "[999B";
+    const get_cursor_pos = esc ++ "[6n";
+    const hide_cursor = esc ++ "?25l";
+    const show_cursor = esc ++ "?25h";
+};
+const TermError = error{
+    get_win_size_failed,
+};
+
+// Structs
+const WindowSize = struct { rows: usize, cols: usize };
+const EditorConfig = struct {
+    orig_termios: std.os.termios,
+    win_size: WindowSize,
+};
+
+// Globals
+var E: EditorConfig = undefined;
+
+fn ctrlKey(c: u8) u8 {
+    return c & 0x1f;
+}
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    var allocator = arena.allocator();
+    defer {
+        _ = STDOUT.write(escape.clear_screen) catch {};
+        _ = STDOUT.write(escape.cursor_to_top) catch {};
+    }
     try enableRawMode();
-    _ = cLibs.atexit(disableRawMode);
+    try initEditor();
 
-    var buf: [1]u8 = undefined;
-    while (try std.io.getStdIn().read(buf[0..]) == 1 and buf[0] != 'q') {
-        const c = buf[0];
-        if (std.ascii.isControl(c)) {
-            std.debug.print("{}\r\n", .{c});
-        } else {
-            std.debug.print("{} ('{c}')\r\n", .{ c, c });
-        }
+    while (true) {
+        try editorRefreshScreen(allocator);
+        if (!try editorProcessKeyPress()) break;
     }
 }
 
-var orig_termios: std.os.termios = undefined;
+fn initEditor() !void {
+    E.win_size = try getWindowSize();
+}
+
+// Terminal
 fn enableRawMode() !void {
-    orig_termios = try std.os.tcgetattr(std.io.getStdIn().handle);
-    var raw = orig_termios;
+    E.orig_termios = try std.os.tcgetattr(std.io.getStdIn().handle);
+    _ = cLibs.atexit(disableRawMode);
+    var raw = E.orig_termios;
 
     const f = std.os.linux;
     raw.iflag &= ~(f.BRKINT | f.ICRNL | f.INPCK | f.ISTRIP | f.IXON);
@@ -27,10 +67,81 @@ fn enableRawMode() !void {
     raw.cflag |= (f.CS8);
     raw.lflag &= ~(f.ECHO | f.ICANON | f.IEXTEN | f.ISIG);
     raw.cc[f.V.MIN] = 0;
-    raw.cc[f.V.TIME] = 1;
+    raw.cc[f.V.TIME] = 50;
 
     try std.os.tcsetattr(std.io.getStdIn().handle, std.os.linux.TCSA.FLUSH, raw);
 }
 fn disableRawMode() callconv(.C) void {
-    std.os.tcsetattr(std.io.getStdIn().handle, std.os.linux.TCSA.FLUSH, orig_termios) catch std.debug.print("Could not reset terminal\r\n", .{});
+    std.os.tcsetattr(std.io.getStdIn().handle, std.os.linux.TCSA.FLUSH, E.orig_termios) catch std.debug.print("Could not reset terminal\r\n", .{});
+}
+
+fn editorReadKey() !u8 {
+    var buf: [1]u8 = undefined;
+    while (try std.io.getStdIn().read(buf[0..]) != 1) {}
+    return buf[0];
+}
+
+fn getWindowSize() !WindowSize {
+    var ws: std.os.linux.winsize = undefined;
+    if (std.os.linux.ioctl(STDOUT.handle, std.os.linux.T.IOCGWINSZ, @intFromPtr(&ws)) != 0 or ws.ws_col == 0) {
+        if (try STDOUT.write(escape.cursor_to_bot ++ escape.cursor_to_right) != 12) return TermError.get_win_size_failed;
+        return getCursorPosition();
+        // return TermError.get_win_size_failed;
+    }
+    return WindowSize{ .rows = ws.ws_row, .cols = ws.ws_col };
+}
+
+fn getCursorPosition() !WindowSize {
+    if (try STDOUT.write(escape.get_cursor_pos) != 4) return TermError.get_win_size_failed;
+    try STDOUT.writer().print("\r\n", .{});
+
+    var i: usize = 0;
+    var buf: [32]u8 = undefined;
+    while (i < buf.len) : (i += 1) {
+        if (try std.io.getStdIn().read(buf[i .. i + 1]) != 1) break;
+        if (buf[i] == 'R') break;
+    }
+
+    if (buf[0] != '\x1b' or buf[1] != '[') return TermError.get_win_size_failed;
+    if (std.mem.indexOf(u8, buf[2..i], ";")) |delimiter| {
+        const d = 2 + delimiter;
+        return WindowSize{ .rows = try std.fmt.parseInt(usize, buf[2..d], 10), .cols = try std.fmt.parseInt(usize, buf[(d + 1)..i], 10) };
+    }
+
+    return TermError.get_win_size_failed;
+}
+
+// Output
+
+fn editorRefreshScreen(allocator: std.mem.Allocator) !void {
+    _ = try STDOUT.write(escape.clear_screen);
+    _ = try STDOUT.write(escape.cursor_to_top);
+
+    var buf = std.ArrayList(u8).init(allocator);
+    defer buf.deinit();
+
+    try buf.appendslice(escape.hide_cursor);
+    try editorDrawRows(&buf);
+    try buf.appendSlice(escape.cursor_to_top);
+    try buf.appendSlice(escape.show_cursor);
+    _ = try STDOUT.write(buf.items);
+}
+fn editorDrawRows(buf: *std.ArrayList(u8)) !void {
+    for (0..E.win_size.rows - 1) |_| {
+        try buf.appendSlice("~\r\n");
+    }
+    try buf.append('~');
+    return buf;
+}
+
+// Input
+
+fn editorProcessKeyPress() !bool {
+    const c = try editorReadKey();
+
+    switch (c) {
+        ctrlKey('q') => return false,
+        else => {},
+    }
+    return true;
 }
